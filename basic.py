a786700452ed474859037ba622b8d5f7ad9da230
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2021-06-23 13:44:36 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,123 @@
+# digit constants
+
+DIGITS = '0123456789'
+
+
+# Error class
+class Error:
+    def __init__(self, err_name, details):
+        self.err_name = err_name
+        self.details = details
+
+    def as_string(self):
+        res = f'{self.err_name}: {self.details}'
+        return res
+
+
+class IllegalCharError(Error):
+    def __init__(self, details):
+        super().__init__('Illegal Character', details)
+
+
+# tokens
+TT_INT = 'TT_INT'
+TT_FLOAT = 'FLOAT'
+TT_PLUS = 'PLUS'
+TT_MINUS = 'MINUS'
+TT_MUL = 'MUL'
+TT_DIV = 'DIV'
+TT_LPAREN = 'LPAREN'
+TT_RPAREN = 'RPAREN'
+
+
+class Token:
+    def __init__(self, type_, value=None):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        if self.value:
+            return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+
+# lexer Class
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = -1
+        self.current_char = None
+        self.advance()
+
+    # to advance to nxt character
+    def advance(self):
+        self.pos += 1  # done changes
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in '\t':
+                self.advance()
+
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+                self.advance()
+
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+
+            else:
+                # return error
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError("'" + char + "'")
+
+        return tokens, None
+
+    def make_number(self):
+        num_str = ''
+        dot_count = 0  # to find if its floating point number
+
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
+
+
+# Run
+def run(text):
+    lexer = Lexer(text)
+    tokens, error = lexer.make_tokens()
+    return tokens, error
